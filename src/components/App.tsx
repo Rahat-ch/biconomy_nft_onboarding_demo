f5563cf0cde7bb7db5a7d86d09fb05a09b3160a3
Rahat <rahat@rahatcodes.com>
2023-05-22 12:47:42 +0400

    onboarding demo with nextjs

---
+++ src/components/App.tsx
@@ -0,0 +1,134 @@
+import styles from "../styles/Home.module.css"
+import { useState, useEffect, useRef } from 'react'
+import SocialLogin from "@biconomy/web3-auth"
+import { ChainId } from "@biconomy/core-types";
+import { ethers } from 'ethers'
+import SmartAccount from "@biconomy/smart-account";
+import Minter from './Minter';
+import Spinner from './Spinner';
+
+
+export default function App() {
+  const [smartAccount, setSmartAccount] = useState<SmartAccount | null>(null)
+  const [interval, enableInterval] = useState(false)
+  const sdkRef = useRef<SocialLogin | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [provider, setProvider] = useState<any>(null);
+
+  useEffect(() => {
+    let configureLogin:any
+    if (interval) {
+      configureLogin = setInterval(() => {
+        if (!!sdkRef.current?.provider) {
+          setupSmartAccount()
+          clearInterval(configureLogin)
+        }
+      }, 1000)
+    }
+  }, [interval])
+
+  async function login() {
+    if (!sdkRef.current) {
+      const socialLoginSDK = new SocialLogin()
+      const signature1 = await socialLoginSDK.whitelistUrl('https://biconomysdk-nft-demo.vercel.app/')
+      const signtature2 = await socialLoginSDK.whitelistUrl('http://localhost:4173/')
+      await socialLoginSDK.init({
+        chainId: ethers.utils.hexValue(ChainId.POLYGON_MUMBAI).toString(),
+        network: "testnet",
+        whitelistUrls: {
+          'http://localhost:5173/': signature1,
+          'http://localhost:4173/': signtature2,
+        }
+      })
+      sdkRef.current = socialLoginSDK
+    }
+    if (!sdkRef.current.provider) {
+      sdkRef.current.showWallet()
+      enableInterval(true)
+    } else {
+      setupSmartAccount()
+    }
+  }
+
+  async function setupSmartAccount() {
+    if (!sdkRef?.current?.provider) return
+    sdkRef.current.hideWallet()
+    setLoading(true)
+    const web3Provider = new ethers.providers.Web3Provider(
+      sdkRef.current.provider
+    )
+    setProvider(web3Provider)
+    try {
+      const smartAccount = new SmartAccount(web3Provider, {
+        activeNetworkId: ChainId.POLYGON_MUMBAI,
+        supportedNetworksIds: [ChainId.POLYGON_MUMBAI],
+        networkConfig: [
+          {
+            chainId: ChainId.POLYGON_MUMBAI,
+            dappAPIKey: process.env.NEXT_PUBLIC_BICONOMY_API_KEY,
+          },
+        ],
+      })
+      const acct = await smartAccount.init()
+      console.log({ deployed: await smartAccount.isDeployed(ChainId.POLYGON_MUMBAI)})
+      const isDeployed = await smartAccount.isDeployed(ChainId.POLYGON_MUMBAI)
+      if (isDeployed == false) {
+        console.log("this one needs to be deployed")
+        const deployTx = await smartAccount.deployWalletUsingPaymaster()
+        console.log(deployTx);
+      }
+      setSmartAccount(acct)
+      setLoading(false)
+    } catch (err) {
+      console.log('error setting up smart account... ', err)
+    }
+  }
+
+  const logout = async () => {
+    if (!sdkRef.current) {
+      console.error('Web3Modal not initialized.')
+      return
+    }
+    await sdkRef.current.logout()
+    sdkRef.current.hideWallet()
+    setSmartAccount(null)
+    enableInterval(false)
+  }
+
+  return (
+    <div className={styles.demoContainter}>
+      <h1>Onboard to Account Abstraction</h1>
+      <p>Connect and mint an NFT, no wallet or gas neccasary to get started.</p>
+      <p>Click and deploy a Smart Account - then mint </p>
+      {
+        !smartAccount && !loading && <button className={styles.demoButton} onClick={login}>Login</button>
+      }
+      {
+        loading && (
+          <div>
+            <p>Creating your Smart Account...</p>
+            <Spinner />
+          </div>
+        )
+      }
+      {
+        !!smartAccount && (
+          <div className={styles.buttonWrapper}>
+            <h3>Smart account address:</h3>
+            <p>{smartAccount.address}</p>
+            <Minter smartAccount={smartAccount} provider={provider} loading={loading} />
+            <button className={styles.demoButton} onClick={logout}>Logout</button>
+          </div>
+        )
+      }
+      <br />
+      <div className={styles.linkWrapper}>
+      <a href="https://docs.biconomy.io/introduction/overview" target="_blank" className={styles.readDocs}>
+  Click here to learn more about the Biconomy SDK
+    </a>
+    </div>
+    </div>
+  )
+}
+
+
---
+++ src/components/Minter.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { BigNumber, ethers } from "ethers";
+import abi from "../utils/abi.json";
+import SmartAccount from "@biconomy/smart-account";
+import styles from '../styles/Home.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface Props {
+    smartAccount: SmartAccount
+    provider: any
+    loading: boolean
+  }
+
+const Minter:React.FC<Props> = ({ smartAccount, provider, loading}) => {
+    const [nftContract, setNFTContract] = useState<any>(null)
+    const [nftCount, setNFTCount] = useState<number>(0);
+    const nftAddress = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS || "";
+
+    useEffect(() => {
+      getNFTCount()
+  },[])
+
+    if(loading) {
+        return <div />
+    }
+
+    const getNFTCount= async() => {
+        const contract = new ethers.Contract(
+            nftAddress,
+            abi,
+            provider,
+          )
+        setNFTContract(contract)
+        const count = await contract.balanceOf(smartAccount.address)
+        setNFTCount(count.toNumber());
+    }
+
+    const mintNFT = async () => {
+        try {
+            const infoToast = toast.info('Minting your NFT...', {
+                position: "top-right",
+                autoClose: 25000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+            const mintTx = await nftContract.populateTransaction.mint()
+            const tx1 = {
+            to: nftAddress,
+            data: mintTx.data,
+            }
+            const txResponse = await smartAccount.sendTransaction({ transaction: tx1})
+
+            const txHash = await txResponse.wait();
+            console.log({txHash})
+            
+            getNFTCount()
+            toast.dismiss(infoToast)
+            toast.success('Your NFT has been minted!', {
+                position: "top-right",
+                autoClose: 15000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+        } catch (error) {
+            console.log(error)
+            toast.error('error occured check the console', {
+                position: "top-right",
+                autoClose: 25000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+        }
+    }
+
+    const nftURL = `https://testnets.opensea.io/${smartAccount.address}`
+
+    return(
+        <div>
+            <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+        />
+            <button className={styles.demoButton} onClick={() => mintNFT()}>Mint NFT</button>
+            {nftCount ? (<p>You own {nftCount} tickets </p>): null}
+            {nftCount ? (<p>View your NFTs <a className={styles.viewNFT} href={nftURL} target="_blank">here</a> </p>): null}
+        </div>
+    )
+};
+
+export default Minter;
+
